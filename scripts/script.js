e24c73ce210ab75f8afe3da6f13892e5138c1c2e
Dzmitry Siver <dzmitry.siver@viacomcontractor.com>
2017-04-15 14:38:28 +0300

    News page added. Main page carousel added.

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@
  * Created by Dzmitry_Siver on 4/10/2017.
  */
 
-$(function() {
-    console.log( "ready!" );
+$(function () {
+    console.log("ready!");
 
     $('.business-item').hover(function (e) {
         $(this).addClass('expanded');
@@ -11,11 +11,27 @@ $(function() {
         $(this).removeClass('expanded');
     });
 
+    var galleryElems = document.querySelectorAll('.carousel');
+
+    for (var i = 0, len = galleryElems.length; i < len; i++) {
+        var galleryElem = galleryElems[i];
+        new Flickity(galleryElem, {
+            pageDots: false,
+            wrapAround: true
+        });
+    }
+
+    $('.flickity-prev-next-button').on('click', function (e) {
+        e.preventDefault();
+    });
+
 
     // todo: replace with svg and css color ;
-   $('.service-item').hover(function (e) {
+    $('.service-item').hover(function (e) {
         $(this).find('img').attr('src', 'img/test-icon-white.png');
     }, function () {
         $(this).find('img').attr('src', 'img/test-icon.png');
     });
+
+
 });
